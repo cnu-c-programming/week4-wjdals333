1dcddf6bf0c5424919422643e11cd3b3a9bf3d1b
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-27 16:32:30 +0900

    Update 4-2.c

---
+++ code/4-2/4-2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int sum(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+double average(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    return sum / n;
+}
+
+int max(int arr[], int n) {
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int arr[] = {3, 7, 1, 9, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+printf("sum: %d\n", sum(arr, n));
+printf("avg: %.2f\n", average(arr, n));
+printf("max: %d\n", max(arr, n));
+    return 0;
+}
+
