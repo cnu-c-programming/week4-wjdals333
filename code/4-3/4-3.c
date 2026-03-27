26264cc8188ea228fcb47ef91658d5b911416381
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-27 17:14:55 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -11,7 +11,7 @@ int main() {
         printf("address of arr[%d]: %p\n", i, addr_0);
         printf("adress of arr[%d+1]: %p\n", i, addr_1);
 
-        printf("&(arr[%d]) - &(arr[%d+1]) = %d\n", i, i+1, (unsigned long long)addr_1 - (unsigned long long)addr_0);
+        printf("&(arr[%d]) - &(arr[%d+1]) = %llu\n", i, i+1, (unsigned long long)addr_1 - (unsigned long long)addr_0);
         printf("\n");
     }
     
